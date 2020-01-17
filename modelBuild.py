6ae7f037b54b91a09890a19f12e06fcc4fe4930f
Matt Davis <37150230+Maraudier@users.noreply.github.com>
2020-01-16 20:55:16 -0500

    Pseudo Code

---
+++ modelBuild.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 16 19:45:00 2020
+
+@author: mattdavis
+"""
+
+#def getImports()
+#returns
+
+#def getData() 
+#assign labels
+#returns classCount, labels
+
+# def trainTestSplit()
+#returns trainSet, testSet, validationSet
+
+#def layerClassifier()
+#returns compiled classifier
+
+#def fitGen(classifier, trainSet, validationSet)
+#trainedModel = classifier.fit_generator()
+#returns trainedModel
